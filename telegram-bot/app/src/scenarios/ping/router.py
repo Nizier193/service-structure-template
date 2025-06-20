de2293358bbf733ae570b26b92cd8eb0bc31a417
Nizier193 <nikita.borisov07@gmail.com>
2025-06-20 22:25:16 +0300

    add: telegram bot README; telegram bot functional

--- telegram-bot/app/src/scenarios/ping/router.py
+++ telegram-bot/app/src/scenarios/ping/router.py
@@ -1,11 +1,23 @@
 from telebot import TeleBot
 
+from src.helpers.ping_api import PingAPI
+
+api = PingAPI()
+
 def ping_scenario(bot: TeleBot):
     @bot.message_handler(commands=["ping"])
     def ping(message):
-        bot.send_message(
-            message.chat.id,
-            text="Hi!"
-        )
+        response = api.ping()
+
+        if response:
+            bot.send_message(
+                message.chat.id,
+                text=f"Ping succesfully executed. Ping ID: {response.get("connection", {}).get("id")}."
+            )
+        else:
+            bot.send_message(
+                message.chat.id,
+                text="Something went wrong while executing ping. Probably backend is offline."
+            )
 
     return ping
